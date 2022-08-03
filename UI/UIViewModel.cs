70874acdda5f8aa0149bbc5c329b0af05311fb31
xSouln <90866478+xSouln@users.noreply.github.com>
2022-08-03 12:02:03 +0300

    update

---
+++ UI/UIViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace xLib.UI
+{
+    public abstract class UIViewModel : UINotifyPropertyChanged
+    {
+        public UIElement UIModel { get; set; }
+        public string Name { get; set; }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual object GetParent()
+        {
+            return null;
+        }
+
+        public virtual void Dispose()
+        {
+
+        }
+    }
+}
---
+++ Windows/WindowUIAdapter.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using xLib.UI;
+
+namespace xLib.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowUIAdapter.xaml
+    /// </summary>
+    public partial class WindowUIAdapter : Window
+    {
+        public WindowUIAdapter()
+        {
+            InitializeComponent();
+        }
+
+        public WindowUIAdapter(UIViewModel view_model) : this()
+        {
+            //Grid.SetColumn(view_model.UIModel, 0);
+            //Grid.SetRow(view_model.UIModel, 0);
+
+            //GridControl.Children.Add(view_model.UIModel);
+
+            //view_model.Update();
+        }
+    }
+}
